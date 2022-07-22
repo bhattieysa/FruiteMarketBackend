8ae206b45c80738ba6258f9b2d4f13701402a682
Eysa Azhar <bhattieysa@gmail.com>
2022-07-22 22:38:13 +0500

    Fruite Screen Double query Solve witth promise

--- controller/home_controller/fruits.js
+++ controller/home_controller/fruits.js
@@ -6,51 +6,25 @@ const { pool } = require("../../config/db")
 
 const getFruits = async (req, reply) => {
     const data = req.body;
-    // var sub_array = {}
-
-
-    // for (var i = 1; i <= 3; i++) {
-
-    //     var array = [{
-    //         "name": i,
-    //         "num": i + 1,
-    //         "like": true
-    //     }]
-
-    //     sub_array[i] = array;
-
-
-    // }
-    // console.log(sub_array)
-
-
-
-    pool.query("SELECT * FROM `fruits` WHERE `category_id`=?",
-        [
-            data.category_id
-        ], (error, results, fields) => {
-            const numRows = results.length
-            const fruite_id = results[0].id
-            var super_array = [];
-
-            if (numRows > 0) {
-                var mainObject = {}; // empty Object
-                for (const e of results) {
                     pool.query("SELECT * FROM `fruits` WHERE `category_id`=?",
                         [
                             data.category_id
                         ], async (error, results, fields) => {
                             const numRows = results.length
-                            const fruite_id = results[0].id
+                          
                             var mainObject = {};
                             if (numRows > 0) {     
                                 for  (const e of results) {
                                     try {
-                                       const res = await pool.query("SELECT `id`, `user_id`, `fruite_id` FROM `like_fruite` WHERE `user_id`=? AND `fruite_id`=?",
+                                        // yaha promise islia lagya...phala ya query run ho phr ya return karay and than aga chalay process
+                                        await new Promise((resolve, reject) => {
+                                       pool.query("SELECT `id`, `user_id`, `fruite_id` FROM `like_fruite` WHERE `user_id`=? AND `fruite_id`=?",
                                             [
                                                 data.user_id,
                                                 `${e.id}`
                                             ], (error, results1, fields) => {
+                                                if (error) return reject(error)
+
                                                 if (results1[0] != undefined) {
                                                     var innerObject = [{
                                                         "id": e.id,
@@ -64,9 +38,28 @@ const getFruits = async (req, reply) => {
                                                         "like": true
                                                     }]
                                                     mainObject[e.id] = innerObject;
-                                                    console.log("inside Loop",  mainObject[e.id] )
+                                                    //console.log("inside Loop",  mainObject[e.id] )
+                                                    resolve()
+                                                }else{
+                                                    var innerObject = [{
+                                                        "id": e.id,
+                                                        "name": e.name,
+                                                        "price": e.price,
+                                                        "image": e.image,
+                                                        "unit": e.unit,
+                                                        "category_id": e.category_id,
+                                                        "ratings": e.ratings,
+                                                        "details": e.details,
+                                                        "like": false
+                                                    }]
+                                                    mainObject[e.id] = innerObject;
+                                                    //console.log("inside Loop",  mainObject[e.id] )
+                                                    resolve()
                                                 }
-                                            }) }
+
+                                            })
+                                        })
+                                        }
                                     catch (err) {
                                         console.error(err)
                                     }
@@ -78,20 +71,8 @@ const getFruits = async (req, reply) => {
                                 reply.send(new Error('Fruits Not Exist'))
                             }
 
-
                         })
-
-
-
-                }
-
-
-            } else {
-                reply.send(new Error('Fruits Not Exist'))
-            }
-
-
-        })
+                
 }
 
 
